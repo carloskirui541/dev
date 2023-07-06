4479a7957a89f54552b0e5639f2dc63b2ccfe1ca
carloskirui541 <123838356+carloskirui541@users.noreply.github.com>
2023-07-06 20:45:37 +0000

    almost done

---
+++ webapp/script.js
@@ -0,0 +1,8 @@
+(function checkInput() {
+    var input = document.getElementById('search')
+    var imgEl = document.getElementById('img');
+    if (input.classList.contains('typing')) {
+        input.classList.toggle('not-typing');
+    }
+}
+)()
